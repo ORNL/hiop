6a5b291d0cd0f5ac66b982ad2067caabf8b8cf5d
Tamar DeWilde <tamard19@gmail.com>
2026-06-17 02:01:01 -0400

    Add EVLOSER sparse solver option

--- src/Utils/hiopOptions.cpp
+++ src/Utils/hiopOptions.cpp
@@ -922,7 +922,7 @@ void hiopOptionsNLP::register_options()
   //     - 'gpu' compute mode: work in progress
 
   {
-    vector<string> range{"auto", "ma57", "pardiso", "strumpack", "resolve", "ginkgo", "cusolver-chol"};
+    vector<string> range{"auto", "ma57", "pardiso", "strumpack", "resolve", "evloser", "ginkgo", "cusolver-chol"};
 
     register_str_option("linear_solver_sparse",
                         "auto",
@@ -936,7 +936,7 @@ void hiopOptionsNLP::register_options()
   //  - when GPU mode is on, STRUMPACK is chosen by 'auto' if available
   //  - choosing option ma57 or pardiso with GPU being on, it results in no device being used in the linear solve!
   {
-    vector<string> range{"auto", "ma57", "pardiso", "resolve", "strumpack", "ginkgo"};
+    vector<string> range{"auto", "ma57", "pardiso", "resolve", "evloser", "strumpack", "ginkgo"};
 
     register_str_option("duals_init_linear_solver_sparse",
                         "auto",
@@ -1402,7 +1402,7 @@ void hiopOptionsNLP::ensure_consistence()
   auto kkt_linsys = GetString("KKTLinsys");
   auto sol_sp = GetString("linear_solver_sparse");
   if(kkt_linsys == "full") {
-    if(sol_sp != "resolve" && sol_sp != "pardiso" && sol_sp != "strumpack" && sol_sp != "auto") {
+    if(sol_sp != "resolve" && sol_sp != "evloser" && sol_sp != "pardiso" && sol_sp != "strumpack" && sol_sp != "auto") {
       if(is_user_defined("linear_solver_sparse")) {
         log_printf(hovWarning,
                    "The option 'linear_solver_sparse=%s' is not valid with option 'KKTLinsys=full'. "
@@ -1426,7 +1426,7 @@ void hiopOptionsNLP::ensure_consistence()
   }
 
 #ifndef HIOP_USE_CUDA
-  if(sol_sp == "resolve" || sol_sp == "cusolver-chol") {
+  if(sol_sp == "resolve" || sol_sp == "evloser" || sol_sp == "cusolver-chol") {
     if(is_user_defined("linear_solver_sparse")) {
       log_printf(hovWarning,
                  "The option 'linear_solver_sparse=%s' is not valid without CUDA support enabled."
@@ -1559,7 +1559,8 @@ void hiopOptionsNLP::ensure_consistence()
       }
       set_val("fact_acceptor", "inertia_free");
     }
-  } else if(GetString("linear_solver_sparse") == "strumpack" || GetString("linear_solver_sparse") == "resolve") {
+  } else if(GetString("linear_solver_sparse") == "strumpack" || GetString("linear_solver_sparse") == "resolve" ||
+            GetString("linear_solver_sparse") == "evloser") {
     if(GetString("fact_acceptor") == "inertia_correction") {
       if(is_user_defined("fact_acceptor") && is_user_defined("linear_solver_sparse")) {
         log_printf(hovWarning,
